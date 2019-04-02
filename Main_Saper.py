d2b9636f23df70214dc6e2615cc0df634ccb601c
Bankoff <bartek.fabisiak1@gmail.com>
2019-04-02 20:43:09 +0200

    Przeskalowanie mapy, dodanie scian, zautomatyzowanie systemu chodzenia

--- Main_Saper.py
+++ Main_Saper.py
@@ -6,6 +6,7 @@ import pygame, sys
 from sprites.Bomb import Bomb
 from sprites.Tools import Tools
 from sprites.Saper import Saper
+from sprites.Wall import Wall
 from pygame.locals import *
 
 pygame.init()
@@ -16,21 +17,42 @@ fpsClock = pygame.time.Clock()
 WINDOW_WIDTH = 1000
 WINDOW_HEIGHT = 700
 
-map = [[Saper(), None, None, None, None, Bomb(200, "B"), None],
-       [None, Bomb(200, "A"), None, None, None, None, None],
-       [None, None, None, None, None, None, None],
-       [None, Bomb(300, "C"), None, None, None, None, None],
-       [None, None, None, Bomb(350, "C"), Bomb(850, "A"), None, None],
-       [None, None, Bomb(450, "C"), None, None, None, None],
-       [None, None, None, None, None, None, Bomb(200, "B")],
-       [Tools(), None, None, Bomb(100,"C"), None, None, None],
-       [None, None, None, None, None, Bomb(100, "C"), None],
-       [None, None, Bomb(200, "A"), None, None, None, None]]
+map = [[Wall(), Wall(), Wall(), Wall(), Wall(), Wall(), Wall(), Wall(), Wall(), Wall(), Wall(), Wall(), Wall(), Wall()],
+       [Wall(), Tools(), Wall(), None  , Bomb(980, "A"), Wall(), None  , Wall(), Bomb(1260, "B"), None  , None  , None  , None  , Wall()],
+       [Wall(), None  , Wall(), None  , Wall(), Wall(), None  , Wall(), Wall(), Wall(), Wall(), Wall(), None  , Wall()],
+       [Wall(), None  , Wall(), None  , None  , None  , None  , Wall(), None  , None  , None  , Wall(), None  , Wall()],
+       [Wall(), None  , Wall(), Wall(), Wall(), Wall(), None  , Wall(), None  , None  , None, Wall(), None  , Wall()],
+       [Wall(), None  , Wall(), Bomb(570, "C"), None  , None  , None  , None  , None  , Wall(), None  , Wall(), None  , Wall()],
+       [Wall(), None  , Wall(), Wall(), Wall(), Wall(), Wall(), Wall(), None  , Wall(), None  , Wall(), None  , Wall()],
+       [Wall(), None  , None  , None  , None  , None  , None  , Wall(), None  , Wall(), None  , Wall(), None  , Wall()],
+       [Wall(), Wall(), Wall(), Wall(), Wall(), Wall(), None  , Wall(), None  , Wall(), None  , None  , None  , Wall()],
+       [Wall(), None  , None  , None, None  , None  , None  , Wall(), None  , Wall(), None  , Wall(), Wall(), Wall()],
+       [Wall(), None  , Wall(), Wall(), Wall(), None  , None  , None  , Saper(), Wall(), None  , Wall(), None  , Wall()],
+       [Wall(), None  , Wall(), Bomb(1060, "A"), Wall(), None  , Wall(), None  , Wall(), Wall(), None  , Wall(), None  , Wall()],
+       [Wall(), None  , Wall(), None  , Wall(), None  , Wall(), Wall(), Wall(), None  , None  , None, None  , Wall()],
+       [Wall(), None  , Wall(), None  , Wall(), None  , Wall(), Bomb(1130, "C"), Wall(), None  , Wall(), Wall(), None  , Wall()],
+       [Wall(), None  , Wall(), None  , Wall(), None  , None  , None  , Wall(), None  , Wall(), None  , None  , Wall()],
+       [Wall(), None  , Wall(), None  , Wall(), Wall(), Wall(), Wall(), Wall(), None  , Wall(), None  , Wall(), Wall()],
+       [Wall(), None  , Wall(), None  , Wall(), Bomb(1060, "A"), None  , None  , None  , None  , Wall(), None  , None, Wall()],
+       [Wall(), None  , Wall(), None  , Wall(), Wall(), Wall(), Wall(), Wall(), None  , Wall(), Wall(), None  , Wall()],
+       [Wall(), Bomb(1000, "A"), Wall(), None  , None  , None  , None  , None  , None  , None  , Wall(), Bomb(1100, "B"), None, Wall()],
+       [Wall(), Wall(), Wall(), Wall(), Wall(), Wall(), Wall(), Wall(), Wall(), Wall(), Wall(), Wall(), Wall(), Wall()]]
+
+x = 0
+y = 0
+x_r = 0
+y_r = 0
+
+for i in range(len(map)):
+    for j in range(len(map[i])):
+        if map[i][j].__class__.__name__ == "Saper":
+            x = i
+            y = j
 
 detonated = 0
 defused = 0
 
-Saper_image = pygame.image.load("images/saper_none.png")
+Saper_image = pygame.image.load("images/saper.png")
 Saper_A_image = pygame.image.load("images/saper_A.png")
 Saper_B_image = pygame.image.load("images/saper_B.png")
 Saper_C_image = pygame.image.load("images/saper_C.png")
@@ -39,14 +61,11 @@ Bomb_B_image = pygame.image.load("images/Bomb_B.png")
 Bomb_C_image = pygame.image.load("images/Bomb_C.png")
 Thumbs_up_image = pygame.image.load("images/Thumbs_up.png")
 Hole_image = pygame.image.load("images/Hole.png")
-
 Tool_image = pygame.image.load("images/tools.png")
+Wall_image = pygame.image.load("images/Wall.png")
 
-x = 0
-y = 0
-x_r = 0
-y_r = 0
 
+print(len(map))
 
 # set up the window
 DISPLAYSURF = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT), 0, 32)
@@ -64,7 +83,7 @@ while True:
 
         elif event.type == KEYUP:
             if event.key == K_RIGHT:
-                if x < 9:
+                if x < len(map)-1:
                     x_r = x + 1
                     y_r = y
 
@@ -74,7 +93,7 @@ while True:
                     y_r = y
 
             elif event.key == K_DOWN:
-                if y < 6:
+                if y < len(map[0])-1:
                     y_r = y + 1
                     x_r = x
 
@@ -106,40 +125,44 @@ while True:
             if map[i][j] is not None:
                 if map[i][j].__class__.__name__ == "Saper":
                      if map[i][j].tool == "A":
-                        DISPLAYSURF.blit(Saper_A_image, [i*100, j*100])
+                        DISPLAYSURF.blit(Saper_A_image, [i*50, j*50])
 
                      elif map[i][j].tool == "B":
-                        DISPLAYSURF.blit(Saper_B_image, [i*100, j*100])
+                        DISPLAYSURF.blit(Saper_B_image, [i*50, j*50])
 
                      elif map[i][j].tool == "C":
-                        DISPLAYSURF.blit(Saper_C_image, [i*100, j*100])
+                        DISPLAYSURF.blit(Saper_C_image, [i*50, j*50])
 
                      elif map[i][j].tool == "none":
-                        DISPLAYSURF.blit(Saper_image, [i*100, j*100])
+                        DISPLAYSURF.blit(Saper_image, [i*50, j*50])
 
                 elif map[i][j].__class__.__name__ == "Bomb":
 
                     if map[i][j].time == 0 and map[i][j] != "exploded":
                         map[i][j].type = "exploded"
                         detonated += 1
+
                     if map[i][j].type == "exploded":
-                        DISPLAYSURF.blit(Hole_image, [i * 100, j * 100])
+                        DISPLAYSURF.blit(Hole_image, [i * 50, j * 50])
 
                     elif map[i][j].type == "done":
-                        DISPLAYSURF.blit(Thumbs_up_image, [i * 100, j * 100])
+                        DISPLAYSURF.blit(Thumbs_up_image, [i * 50, j * 50])
 
                     elif map[i][j].type == "A":
-                        DISPLAYSURF.blit(Bomb_A_image, [i*100, j*100])
+                        DISPLAYSURF.blit(Bomb_A_image, [i * 50, j * 50])
 
                     elif map[i][j].type == "B":
-                        DISPLAYSURF.blit(Bomb_B_image, [i*100, j*100])
+                        DISPLAYSURF.blit(Bomb_B_image, [i * 50, j * 50])
 
                     elif map[i][j].type == "C":
-                        DISPLAYSURF.blit(Bomb_C_image, [i*100, j*100])
+                        DISPLAYSURF.blit(Bomb_C_image, [i * 50, j * 50])
                     map[i][j].tick()
 
                 elif map[i][j].__class__.__name__ == "Tools":
-                    DISPLAYSURF.blit(Tool_image, [i * 100, j * 100])
+                    DISPLAYSURF.blit(Tool_image, [i * 50, j * 50])
+
+                elif map[i][j].__class__.__name__ == "Wall":
+                    DISPLAYSURF.blit(Wall_image, [i * 50, j * 50])
 
     # Refresh Screen
     pygame.display.flip()
